478f482cd247cdd5f021bde3edbfa22b8595be53
Danny Mayer <danny@dannys-mbp.lan>
2020-09-12 07:46:09 -0700

    Added Social Share content

--- functions.php
+++ functions.php
@@ -106,6 +106,38 @@ function my_custom_scripts(){
 add_action('wp_enqueue_scripts', 'my_custom_scripts');
 
 
+//Adds the Open Graph in the Language Attributes
+function add_opengraph_doctype( $output ) {
+        return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
+    }
+add_filter('language_attributes', 'add_opengraph_doctype');
+ 
+//Lets add Open Graph Meta Info
+ 
+function insert_fb_in_head() {
+    global $post;
+    if ( !is_singular()) //if it is not a post or a page
+        return;
+       // echo '<meta property="fb:app_id" content="Your Facebook App ID" />';
+        echo '<meta property="og:title" content="Online Bible Study | Compass Bible Church "/>';
+        echo '<meta property="og:type" content="article"/>';
+        echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+		echo '<meta property="og:description" content="The various ministry bible studies are designed to get you personally connected on a weekly basis with other Christians in order to help you advance in your understanding and application of biblical themes that are critically important in the Christian life." />';
+        echo '<meta property="og:site_name" content="Compass Bible Church Online Study"/>';
+    if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
+        $default_image="https://compassbible.study/wp-content/uploads/2020/09/social-share.png"; //replace this with a default image on your server or an image in your media library
+        echo '<meta property="og:image" content="' . $default_image . '"/>';
+    }
+    else{
+        $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+        echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+    }
+    echo "
+";
+}
+add_action( 'wp_head', 'insert_fb_in_head', 5 );
+
+
 // Adds Version Number to Stylesheet, to help with Caching, will be removed at launch
 add_action( 'wp_enqueue_scripts', function() {
 
